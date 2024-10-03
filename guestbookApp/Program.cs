2e75048238509ab388a537ec26049adc38feac29
Kajsa Classon <ispyspiders@gmail.com>
2024-10-03 11:49:00 +0200

    Case 2 - ta bort inlägg: lagt till koll om valt index finns i gästbok och en bekräftelse-dialog för borttagning

--- guestbookApp/Program.cs
+++ guestbookApp/Program.cs
@@ -38,27 +38,47 @@ namespace guestbook
                 {
                     case '1': //Skriv i gästboken
                         Console.CursorVisible = true; // Tänd cursor
-                        Console.Write("Namn: ");
+                        Console.Write("\nNamn: ");
                         string? name = Console.ReadLine(); // Läs in namn och spara i variabel
                         Console.Write("Meddelande: ");
                         string? msg = Console.ReadLine(); // Läs in meddelande och spara i variabel
-                        // Kontrollera att strängar inte är null eller tomma och lägg i så fall till inlägg i gästbok
-                        if (!String.IsNullOrEmpty(name) && !String.IsNullOrEmpty(msg)) guestbook.AddPost(name, msg);
+                        if (!String.IsNullOrEmpty(name) && !String.IsNullOrEmpty(msg)) // Kontrollera att strängar inte är null eller tomma
+                        {
+                            guestbook.AddPost(name, msg); //  och lägg i så fall till inlägg i gästbok
+                        }
                         break;
                     case '2': // Ta bort ett inlägg
                         Console.CursorVisible = true; // Tänd cursor
-                        Console.Write("Ange index för meddelandet du vill ta bort: ");
+                        Console.Write("\nAnge index för meddelandet du vill ta bort: ");
                         string? index = Console.ReadLine(); // Läs in index och spara i sträng
-                        if (!String.IsNullOrEmpty(index))
-                            try
+                        if (!String.IsNullOrEmpty(index)) // Kontrollera att sträng inte är tom eller null
+                        {
+                            if (Int32.TryParse(index, out int result)) // Kontrollera att index är ett heltal
                             {
-                                guestbook.delPost(Convert.ToInt32(index));
+                                int postCount = guestbook.GetPosts().Count(); // Antal inlägg i gästbok
+                                if (result >= 0 && result <= postCount - 1) // Om valt index är ett positivt tal och mindre än eller samma som antal inlägg i gästboken
+                                {
+                                    Console.CursorVisible = false; // Släck cursor
+                                    Console.WriteLine($"Är du säker på att du vill ta bort meddelandet med index [{index}]?\n");
+                                    Console.WriteLine("1. JA");
+                                    Console.WriteLine("2. NEJ\n");
+
+                                    int inp = (int)Console.ReadKey(true).Key; // Läs in input
+                                    if (inp == '1') // Om input är 1 försök ta bort inlägg med valt index
+                                    {
+                                        try
+                                        {
+                                            guestbook.delPost(Convert.ToInt32(index));
+                                        }
+                                        catch (Exception)
+                                        {
+                                            Console.WriteLine("Index out of range! Vi hittar inget meddelande med valt index.\nTryck på valfri tangent för att fortsätta.");
+                                            Console.ReadKey();
+                                        }
+                                    }
+                                }
                             }
-                            catch (Exception)
-                            {
-                                Console.WriteLine("Index out of range! Vi hittar inget meddelande med valt index.\nTryck på valfri tangent för att fortsätta.");
-                                Console.ReadKey();
-                            }
+                        }
                         break;
                     case 88: // X för exit
                         Environment.Exit(0);
